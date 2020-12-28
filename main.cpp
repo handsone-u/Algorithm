b538c1d6ad08624c5642090ad28a5b891375c016
handsone-u <jk04172@naver.com>
2020-12-28 17:30:02 +0900

    2579

--- main.cpp
+++ main.cpp
@@ -7,27 +7,20 @@
 
 #include <iostream>
 using namespace std;
-#define MIN(a,b) (((a) < (b)) ? (a) : (b))
-int dp[5001];
+int dp[41],t,n;
 
 int main(int argc, const char * argv[]) {
+    cin>>t;
+    dp[0]=0;
+    dp[1]=dp[2]=1;
     
-    int x,i,l,r;
-    cin >> x;
-    dp[1]=dp[2]=dp[4]=-1;
-    dp[3]=dp[5]=1;
+    for(int i=3;i<=40;i++) dp[i]=dp[i-1]+dp[i-2];
     
-    for(i=6;i<=x;i++)
-    {
-        l = dp[i-3]+1;
-        r = dp[i-5]+1;
-        if(!l&&!r) dp[i] = -1;
-        else if(l&&!r) dp[i] = l;
-        else if(!l&&r) dp[i] = r;
-        else dp[i] = MIN(l,r);
+    while(t--){
+        cin>>n;
+        if(n==0) cout<<"1 0\n";
+        else if(n==1) cout<<"0 1\n";
+        else cout<<dp[n-1]<<" "<<dp[n]<<"\n";
     }
-    
-    cout<<dp[x]<<endl;
-    
     return 0;
 }
