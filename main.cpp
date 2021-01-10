72c3aad0109b737980b53d7d7e0bace29ba85613
handsone-u <jk04172@naver.com>
2021-01-10 19:45:54 +0900

    1

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
-//
-//  main.cpp
-//  algorithm
-//
-//  Created by handsone on 2020/12/27.
-//
-
 #include <iostream>
+#include <string>
 using namespace std;
-int dp[41],t,n;
-
-int main(int argc, const char * argv[]) {
-    cin>>t;
-    dp[0]=0;
-    dp[1]=dp[2]=1;
-    
-    for(int i=3;i<=40;i++) dp[i]=dp[i-1]+dp[i-2];
-    
-    while(t--){
-        cin>>n;
-        if(n==0) cout<<"1 0\n";
-        else if(n==1) cout<<"0 1\n";
-        else cout<<dp[n-1]<<" "<<dp[n]<<"\n";
-    }
+int b,m;
+int a[26],c,d;
+string str;
+int main()
+{
+    for(int i=0;i<10;i++)
+        cout << i << endl;
     return 0;
 }
