9c3e8588ac582947e1f4f027321c13ba2d3a9867
handsone-u <jk04172@naver.com>
2021-03-28 22:32:06 +0900

    210328

--- DP/13301.cpp
+++ DP/13301.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+long long dp[81];
 using namespace std;
-int d[3];
 int main(){
-    cout<<1<<endl<<"Asdad";
-    for(int i=0;i<10;i++)
-        cout<<d[i]<<' ';
+    dp[1]=1;
+    dp[2]=1;
+    int n;
+    cin>>n;
+    for(int i=3;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+    cout<<dp[n]*2+(dp[n]+dp[n-1])*2<<'\n';
     return 0;
 }
