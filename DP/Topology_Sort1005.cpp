6491f40333ed8acdfc19b5e1b4b46b50c7239c59
handsone-u <jk04172@naver.com>
2021-04-15 21:44:46 +0900

    210415

--- DP/Topology_Sort1005.cpp
+++ DP/Topology_Sort1005.cpp
@@ -1,41 +1,42 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
 int t,n,k,w;
+int d[1001],pion[1001];
 int main(){
     ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin>>t;
     while(t--){
-        int b[1001]={0,},d[1001]={0,};
-        bool pion[1001]={0,};
+        int b[1001]={0,};
         vector<int> node[1001];
         queue<int> q;
         cin>>n>>k;
         for(int i=1;i<=n;i++) cin>>b[i];
+        fill_n(d,n+1,0);
+        fill_n(pion,n+1,0);
         for(int i=0;i<k;i++){
             int x,y;
             cin>>x>>y;
             node[x].push_back(y);
-            pion[y]=true;
+            pion[y]++;
         }
         cin>>w;
-        for(int i=1;i<=n;i++){
-            if(pion[i]) continue;
-            q.push(i);
+        for(int i=1;i<=n;i++) if(pion[i]==0) {
             d[i]=b[i];
+            q.push(i);
         }
         while(!q.empty()){
-            int x = q.front();
+            int x = q.front();q.pop();
             int vsize = node[x].size();
-            q.pop();
             for(int i=0;i<vsize;i++){
                 int tnode = node[x][i];
+                if(pion[tnode]) pion[tnode]--;
+                if(pion[tnode]==0) q.push(tnode);
                 int tmp = b[tnode]+d[x];
-                if(d[tnode]<tmp) {
+                if(d[tnode]<tmp)
                     d[tnode]=tmp;
-                    q.push(tnode);
-                }
             }
         }
         cout<<d[w]<<'\n';
